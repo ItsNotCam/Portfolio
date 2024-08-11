a7d6a3ab785ce9bfbf92411ec54aba2a00f9bd5a
ItsNotCam <camallanyoung@gmail.com>
2024-08-10 21:11:15 -0400

    changed - reduced project description verbosity

--- src/data/project_list.tsx
+++ src/data/project_list.tsx
@@ -39,7 +39,7 @@ export const ProjectList: Project[] = [{
     icon: <Tailwind_CSSIcon />,
   }],
   content: <span>
-    This fully responsive website was designed and written by me from scratch.<br/><br/>
+    This fully responsive website was designed and written by me from scratch.
     It was designed 
     in <a target="_blank" href="https://www.figma.com/design/nkOThbhroZHoa0L8h44F5v/Portfolio-v2?node-id=0-1&t=awrN6I3LWpUGJbK9-1" 
       className="text-custom-blue/90 underline hover:text-blue-300 transition-colors duration-200">Figma</a> and 
@@ -75,7 +75,7 @@ export const ProjectList: Project[] = [{
     name: "SQL Server",
     icon: <SQLServerIcon />,
   }],
-  content: <span>A full stack web application built for BAE Systems, Inc.<br/><br/> 
+  content: <span>A full stack web application built for BAE Systems, Inc.
   It displays essential information and statistics for an internal account and password management application.<br/>
   </span>
 }, {
@@ -96,7 +96,7 @@ export const ProjectList: Project[] = [{
     name: "Tailwind",
     icon: <Tailwind_CSSIcon />,
   }],
-  content: <span>This front-end web application was developed for BAE Systems, Inc.<br/><br/> 
+  content: <span>This front-end web application was developed for BAE Systems, Inc.
   It integrates with an internal AI chatbot LLM to assist software developers by offering feedback, generating code snippets, and more.<br/></span>
 }, {
   name: "🚀Pokescrape",
@@ -111,9 +111,10 @@ export const ProjectList: Project[] = [{
     name: "PostgreSQL",
     icon: <PostgreSQLIcon />,
   }],
-  content: <span>This is a web scraper designed to extract Pokémon data from 'pokemondb.net' and store it in a PostgreSQL database.<br/><br/> 
-  The data is structured with complex relationships, allowing for highly detailed filtering and searching. <br/><br/>
-  For more information, including the Entity Relationship Diagram (ERD) and additional documentation, refer to the README by clicking on the title of this card.<br/></span>
+  content: <span>This is a web scraper designed to extract Pokémon data from 'pokemondb.net' and store it in a PostgreSQL database.</span>
+	// <br/><br/> 
+  // The data is structured with complex relationships, allowing for highly detailed filtering and searching. <br/><br/>
+  // For more information, including the Entity Relationship Diagram (ERD) and additional documentation, refer to the README by clicking on the title of this card.<br/></span>
 }, {
   name: "🌏Cloud File Share",
   githubLink: "https://github.com/ItsNotCam/GoogleDriveClone",
@@ -147,10 +148,10 @@ export const ProjectList: Project[] = [{
     icon: <MySQLIconSmall height="2rem" width="2rem"/>,
   },],
   content: <span>
-    A containerized full-stack web application that replicates the core functionality found in cloud file-sharing platforms like Google Drive.<br/><br/> 
-    It features a very sleek UI, allowing users to create accounts, upload and download files, and manage file access by granting or revoking permissions to other users.<br/><br/>
-     To see the demo, click the globe icon at the top of the card.<br/></span>
-
+    A containerized full-stack web application that replicates the core functionality found in cloud file-sharing platforms like Google Drive.</span>
+		// <br/><br/> 
+    // It features a very sleek UI, allowing users to create accounts, upload and download files, and manage file access by granting or revoking permissions to other users.<br/><br/>
+    //  To see the demo, click the globe icon at the top of the card.<br/></span>
 }, {
   name: "💸Pico",
   githubLink: "https://github.com/ItsNotCam/Pico",
@@ -174,11 +175,12 @@ export const ProjectList: Project[] = [{
     name: "TypeScript",
     icon: <TypeScriptIcon />,
   }],
-  content: <span>This project is an HTML, CSS, and jQuery-based website showcasing a mock smartphone advertisement.<br/><br/>
-  It represents my first attempt at creating a purely static site with a strong emphasis on CSS, including some responsive features.<br/><br/>
-  The website was developed as part of a school project, and during my class presentation the professor stopped my presentation twice
-  to confirm that I had actually created the site myself, as they believed that it was too professional to have been made by a student.<br/>
-  </span>
+  content: <span>This project is my first attempt at creating an HTML, CSS, and jQuery-based website. Completed for a school project.</span>
+	// <br/><br/>
+  // It represents my first attempt at creating a purely static site with a strong emphasis on CSS, including some responsive features.<br/><br/>
+  // The website was developed as part of a school project, and during my class presentation the professor stopped my presentation twice
+  // to confirm that I had actually created the site myself, as they believed that it was too professional to have been made by a student.<br/>
+  // </span>
 },{
   name: "🔥HeaterPi",
   githubLink: "https://github.com/ItsNotCam/HeaterPi-Server",
@@ -201,8 +203,9 @@ export const ProjectList: Project[] = [{
     name: "ReactJS",
     icon: <ReactJSIcon />,
   }],
-  content: <span>A full-stack web application that manages room temperature using a Raspberry Pi and a standard space heater.<br/><br/> 
-  It features a sleek front end for easy viewing of live information and uses WebSockets for real-time communication between the client and the device.<br/></span>
+  content: <span>A full-stack web application that manages room temperature using a Raspberry Pi and a standard space heater.</span>
+	// <br/><br/> 
+  // It features a sleek front end for easy viewing of live information and uses WebSockets for real-time communication between the client and the device.<br/></span>
 },  {
   name: "⌚Bitwatch VR",
   githubLink: "https://github.com/ItsNotCam/BitwatchVR",
@@ -213,9 +216,10 @@ export const ProjectList: Project[] = [{
     name: "C#",
     icon: <CSharpIcon />,
   }],
-  content: <span>A virtual reality smartwatch that bridges the real and virtual worlds, displaying real-time information such as the user's local time, weather, heart rate, and more directly on their virtual wrist.<br/><br/>
-  Fully interactive within a virtual reality environment, the project involved numerous challenges whose solutions showcase both my creativity and problem-solving skills.<br/><br/> 
-  For a detailed overview including explanations for some of these challenges and my solutions, click the title of this card.<br/></span>
+  content: <span>A virtual reality smartwatch that bridges the real and virtual worlds, displaying real-time information such as the user's local time, weather, heart rate, and more directly on their virtual wrist.</span>
+	// <br/><br/>
+  // Fully interactive within a virtual reality environment, the project involved numerous challenges whose solutions showcase both my creativity and problem-solving skills.<br/><br/> 
+  // For a detailed overview including explanations for some of these challenges and my solutions, click the title of this card.<br/></span>
 }, {
   name: "📦Coordinates Book",
   githubLink: "https://github.com/ItsNotCam/MC-Coordinates-Book",
@@ -229,8 +233,9 @@ export const ProjectList: Project[] = [{
     name: "SQLite",
     icon: <SQLiteIcon />,
   }],
-  content: <span>A modification for the popular video game Minecraft that automates the process of saving your favorite locations within the game, replacing traditional (and literal) pen-and-paper methods.<br/><br/> 
-  It utilizes the game's limited resources to create a user interface that seamlessly integrates with the game's existing interfaces, providing a faster and more intuitive way for players to store this important information.<br/></span>
+  content: <span>A modification for the popular video game Minecraft that automates the process of saving your favorite locations within the game, replacing traditional (and literal) pen-and-paper methods.</span>
+	// <br/><br/> 
+  // It utilizes the game's limited resources to create a user interface that seamlessly integrates with the game's existing interfaces, providing a faster and more intuitive way for players to store this important information.<br/></span>
 }, {
   name: "📑Home File Server",
   githubLink: "https://github.com/ItsNotCam/File-Server",
