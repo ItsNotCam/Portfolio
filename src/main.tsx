3280128cb5b15d8d57db4218afb7b48c747514a7
ItsNotCam <camallanyoung@gmail.com>
2024-08-25 20:39:43 -0400

    added custom cursor

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React, { ReactNode } from "react";
+import React, { CSSProperties, ReactNode } from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./routes/home/HomeRoute.tsx";
 
+import { useState } from "react";
 import {
   createBrowserRouter,
   Navigate,
@@ -17,7 +18,20 @@ import Work from "./routes/work/WorkRoute.tsx";
 import Resume from "./routes/resume/ResumeRoute.tsx";
 import Projects from "./routes/projects/ProjectsRoute.tsx";
 
-import "./globals.css";
+export default function CustomCursor(): JSX.Element {
+	const [mousePos, setMousePos] = useState<CSSProperties>({ left: 0, top: 0 })
+
+	document.addEventListener('mousemove', (e: MouseEvent) => {
+		setMousePos({ left: e.pageX, top: e.pageY });
+	})
+
+	return (
+		<div className={
+			`fixed -translate-x-1/2 -translate-y-1/2 h-6 w-6 border-2 border-custom-text-100 rounded-full pointer-events-none overflow-hidden` 
+		} style={{...mousePos, zIndex: 999999}}
+		/>
+	)
+}
 
 const Wrapper = (props: { children?: ReactNode }): ReactNode => (
   <div className="flex flex-col justify-between h-full w-full">
@@ -26,6 +40,7 @@ const Wrapper = (props: { children?: ReactNode }): ReactNode => (
     {props.children}
   </div>
 );
+
 
 export const routes = [
   {
@@ -94,6 +109,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+		<CustomCursor />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
