d0013b1d970df283c0192ec45dc92e2e4cdb2a5b
ItsNotCam <camallanyoung@gmail.com>
2024-08-25 21:05:37 -0400

    added hover effects to custom cursor

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -2,15 +2,23 @@ import { CSSProperties, useState } from "react";
 
 export default function CustomCursor(): JSX.Element {
 	const [mousePos, setMousePos] = useState<CSSProperties>({ left: 0, top: 0 })
+	const [isHoveredOverHoverable, setIsHoveredOverHoverable] = useState<boolean>(false);
 
 	document.addEventListener('mousemove', (e: MouseEvent) => {
+		const hoveredElement = document.elementFromPoint(e.clientX, e.clientY);
+		if (hoveredElement) {
+			setIsHoveredOverHoverable(window.getComputedStyle(hoveredElement).cursor === 'pointer');
+		} else {
+			setIsHoveredOverHoverable(false);
+		}
+	
 		setMousePos({ left: e.pageX, top: e.pageY });
 	})
 
 	return (
-		<div className={
-			`fixed -translate-x-1/2 -translate-y-1/2 h-6 w-6 border-2 border-custom-text-100 rounded-full pointer-events-none overflow-hidden` 
-		} style={{...mousePos, zIndex: 999999}}
+		<div className=
+			{ `fixed -translate-x-1/2 -translate-y-1/2 h-6 w-6 border-2 border-custom-text-100 rounded-full pointer-events-none overflow-hidden transition-transform ${isHoveredOverHoverable ? 'scale-150' : ''}`} 
+			style={{...mousePos, zIndex: 999999}}
 		/>
 	)
 }
